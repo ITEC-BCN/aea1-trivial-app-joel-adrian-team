a739fa02a6344d2fa3e14606946cff90f26d53c5
jlariosm-itb <joel.larios.7e9@itb.cat>
2026-01-15 10:08:36 +0100

    Initial commit: Created routes and Hello world on screens.

--- app/src/main/java/com/example/trivialapp_base/Routes.kt
+++ app/src/main/java/com/example/trivialapp_base/Routes.kt
@@ -1,5 +1,8 @@
 package com.example.trivialapp_base
 
 sealed class Routes(val route: String) {
-
+    object Splash : Routes("splash")
+    object menu : Routes("menu")
+    object game : Routes("game")
+    object score : Routes("score")
 }
--- app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_result.kt
@@ -1,10 +1,22 @@
 package com.example.trivialapp_base.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun ResultScreen(navController: NavController, viewModel: GameViewModel) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Green)) {
+        Text(text = "Pantalla result", modifier = Modifier.align(Alignment.Center))
+    }
 
 }
