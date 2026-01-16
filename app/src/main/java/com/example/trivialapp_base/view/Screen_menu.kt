1206c293c31da1d5a299fa8540f63737bd074d1f
AdrianVillodres <adrian.villodres.7e8@itb.cat>
2026-01-16 09:17:16 +0100

    feat:screen change feature done, clicking the text you can change the screen

--- app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_menu.kt
@@ -1,8 +1,14 @@
 package com.example.trivialapp_base.view
 
+import android.R.attr.bottom
+import android.R.attr.end
+import android.R.attr.start
+import android.R.attr.top
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,12 +21,21 @@ import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun MenuScreen(navController: NavController, viewModel: GameViewModel) {
-    Box(
-        modifier = Modifier.fillMaxSize().background(Color.Green)) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         Text(
-            text = "Pantalla 2",
-            modifier = Modifier
-                .align(Alignment.Center)
-                .clickable() { navController.navigate(Routes.game.route) })
+            text = "Main Menu"
+
+        )
+        Box(modifier = Modifier) {
+            Text(
+                text = "Pantalla 2",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .clickable() { navController.navigate(Routes.game.route) })
+        }
     }
 }
