3d413a60b448777e2fc71f84e7f331052b7769f6
jlariosm-itb <joel.larios.7e9@itb.cat>
2026-01-16 10:44:51 +0100

    feat: Game screen started

--- app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
@@ -1,25 +1,52 @@
 package com.example.trivialapp_base.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.trivialapp_base.Routes
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun GameScreen(navController: NavController, viewModel: GameViewModel) {
+    var round by remember { mutableStateOf(1) }
+    val totalRounds = 10 // Número total de rondas
+
     Box(
-        modifier = Modifier.fillMaxSize().background(Color.Green)) {
+        modifier = Modifier.fillMaxSize().background(Color.Black)) {
         Text(
-            text = "Pantalla 3",
-            modifier = Modifier
-                .align(Alignment.Center)
-                .clickable() { navController.navigate(Routes.score.route) })
+            text = "Round $round/$totalRounds",
+            modifier = Modifier.align(Alignment.TopCenter),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = viewModel.preguntaActual?.pregunta ?: "",
+            fontSize = 22.sp,
+            modifier = Modifier.padding(bottom = 24.dp)
+        )
+    }
     }
 }
