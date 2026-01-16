ae75f31df1c59a000eb703932ceca7c23f035e4c
jlariosm-itb <joel.larios.7e9@itb.cat>
2026-01-16 10:44:51 +0100

    feat: Game screen started

--- app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
@@ -1,7 +1,6 @@
 package com.example.trivialapp_base.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
