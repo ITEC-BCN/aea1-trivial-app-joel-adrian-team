a44aa563db089bc8e8658a9fb1d977929ec4d030
jlariosm-itb <joel.larios.7e9@itb.cat>
2026-01-21 10:40:09 +0100

    feat: Added Rounds, Questions and Answers buttons to the game screen.

--- app/src/main/java/com/example/trivialapp_base/MainActivity.kt
+++ app/src/main/java/com/example/trivialapp_base/MainActivity.kt
@@ -5,13 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -21,7 +14,6 @@ import com.example.trivialapp_base.ui.theme.TrivialAPP_BaseTheme
 import com.example.trivialapp_base.view.GameScreen
 import com.example.trivialapp_base.view.MenuScreen
 import com.example.trivialapp_base.view.ResultScreen
-import com.example.trivialapp_base.view.SplashScreen
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
 class MainActivity : ComponentActivity() {
